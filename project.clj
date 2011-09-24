b73fdcda0939756366fc039fd92e3044f387bf8e
Arthur Edelstein <arthuredelstein@gmail.com>
2011-09-23 20:58:48 -0700

    move to clojure 1.3.0 and bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject local-file "0.0.6"
+(defproject local-file "0.1.0"
   :description "read files local to a project in clojure"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[lein-clojars "0.7.0"]])
